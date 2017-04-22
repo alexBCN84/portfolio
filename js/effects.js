ecdf0d5e1468e53630984447e8cf5b7df81ff4b2
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2017-04-22 22:52:39 +0200

    templateViews created and testimonials added

--- js/effects.js
+++ js/effects.js
@@ -6,7 +6,6 @@ jQuery(function($) {
       this.typewriter();
       this.smoothVerticalScroll();
       this.smoothHorizontalScroll();
-      this.codeGalleryTypewriter();
     },
     typewriter: function() {
       var TxtType = function(el, toRotate, period) {
@@ -96,78 +95,6 @@ jQuery(function($) {
         return false;
       });
     },
-
-    codeGalleryTypewriter: function() {
-      var codeGallery = {
-  setupTypewriter: function(t) {
-    var HTML = t.innerHTML;
-    t.innerHTML = '';
-
-    var cursorPosition = 0,
-        tag = '',
-        writingTag = false,
-        tagOpen = false,
-        typeSpeed = 100,
-        tempTypeSpeed = 0;
-
-    var type = function() {
-      if (writingTag === true) {
-        tag += HTML[cursorPosition];
-      }
-
-      if (HTML[cursorPosition] === "<") {
-        tempTypeSpeed = 0;
-        if (tagOpen) {
-          tagOpen = false;
-          writingTag = true;
-        } else {
-          tag = "";
-          tagOpen = true;
-          writingTag = true;
-          tag += HTML[cursorPosition];
-        }
-      }
-      if (!writingTag && tagOpen) {
-        tag.innerHTML += HTML[cursorPosition];
-      }
-      if (!writingTag && !tagOpen) {
-        if (HTML[cursorPosition] === " ") {
-          tempTypeSpeed = 0;
-        } else {
-          tempTypeSpeed = (Math.random() * typeSpeed) + 50;
-        }
-        t.innerHTML += HTML[cursorPosition];
-      }
-      if (writingTag === true && HTML[cursorPosition] === ">") {
-        tempTypeSpeed = (Math.random() * typeSpeed) + 50;
-        writingTag = false;
-        if (tagOpen) {
-          var newSpan = document.createElement("span");
-          t.appendChild(newSpan);
-          newSpan.innerHTML = tag;
-          tag = newSpan.firstChild;
-        }
-      }
-
-      cursorPosition += 1;
-      if (cursorPosition < HTML.length - 1) {
-        setTimeout(type, tempTypeSpeed);
-      }
-    };
-    return {
-      type: type
-    };
   }
-}
-  var typewriter = document.getElementById('typewriter');
-
-  typewriter = codeGallery.setupTypewriter(typewriter);
-
-  typewriter.type();
-
-    }
-
-  }
-
   effects.innit();
 });
