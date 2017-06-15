4e43a042b7aaa0d0b7ee3260015385a3d38766e4
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2017-06-15 17:10:21 +0200

    fully responsive front-page

--- js/landingBlock_template.js
+++ js/landingBlock_template.js
@@ -5,14 +5,10 @@ const template = `<div id="front-page">
           <ul class="intro-list">
             <li id="subheading">I'm passionate about the web and I proudly turn my passion into my career</li>
           </ul>
-          <ul>
-            <li><a href="#testimonials" class="button">testimonials</a></li>
-            <li><a href="#featured-code" class="button">featured code</a></li>
+          <ul id="more-buttons">
+            <li><a href="#testimonials" id="btn-testimonials" class="button">testimonials</a></li>
+            <li><a href="#featured-code" id="btn-featured-code" class="button">featured code</a></li>
           </ul>
         </div>`
 
 document.getElementById('landingBlock').innerHTML += template;
-
-
-
-
